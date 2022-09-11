23ef6e78a3a72e077dfac56abb4902b1a8381b61
Stephen Bernard <stephenbenard3@gmail.com>
2022-09-11 22:21:55 +0000

    Last one

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+
+/* File: 8.c */
+/* Auth: Stephen Bernard */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Start Point
+ * Description: Prints all single digit numbers of base 16
+ * Starting from 0, followed by a new line.
+ * Function allowed: only putchar
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar(x + '0');
+		if (x !=  9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+
