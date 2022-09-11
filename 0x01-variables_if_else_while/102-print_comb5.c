e6433ec92c3b6f1205a4c48c481ce6d452decc93
Stephen Bernard <stephenbenard3@gmail.com>
2022-09-11 22:35:02 +0000

    I am done Boomni baby. Thanks to you

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print single digit numbers starting from 0
+ *
+ * Return: returns zero at the end
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if (i < j)
+			{
+				putchar(i / 10 + 48);
+				putchar(i % 10 + 48);
+				putchar(32);
+				putchar(j / 10 + 48);
+				putchar(j % 10 + 48);
+				if (i < 98)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
